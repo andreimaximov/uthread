393237e11a513e2951ce0387c4b8cbddae26fd8a
Andrei Maximov <andrei.maximov@gmail.com>
2018-07-07 14:57:02 -0800

    Include examples in clang-format script

--- scripts/clang-format.sh
+++ scripts/clang-format.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-find                               \
-    "${MESON_SOURCE_ROOT}/bin"     \
-    "${MESON_SOURCE_ROOT}/src"     \
-    "${MESON_SOURCE_ROOT}/include" \
-    "${MESON_SOURCE_ROOT}/test"    \
-    "${MESON_SOURCE_ROOT}/bench"   \
+find                                \
+    "${MESON_SOURCE_ROOT}/examples" \
+    "${MESON_SOURCE_ROOT}/src"      \
+    "${MESON_SOURCE_ROOT}/include"  \
+    "${MESON_SOURCE_ROOT}/test"     \
+    "${MESON_SOURCE_ROOT}/bench"    \
     -iname '*.cpp' -o -iname '*.hpp' | xargs clang-format -i -style=file
 
