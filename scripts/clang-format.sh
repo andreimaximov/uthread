f198d111d6af11c918a555908157004fd70621ed
Andrei Maximov <andrei.maximov@gmail.com>
2019-04-06 12:54:31 -0700

    Cleanup clang-format script

--- scripts/clang-format.sh
+++ scripts/clang-format.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
 find                                \
-    "${MESON_SOURCE_ROOT}/examples" \
-    "${MESON_SOURCE_ROOT}/src"      \
-    "${MESON_SOURCE_ROOT}/include"  \
-    "${MESON_SOURCE_ROOT}/test"     \
     "${MESON_SOURCE_ROOT}/bench"    \
+    "${MESON_SOURCE_ROOT}/examples" \
+    "${MESON_SOURCE_ROOT}/include"  \
+    "${MESON_SOURCE_ROOT}/src"      \
+    "${MESON_SOURCE_ROOT}/test"     \
     -iname '*.cpp' -o -iname '*.hpp' | xargs clang-format -i -style=file
+
